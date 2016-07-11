de28b2c3b1b8c4db02916f082e90856f3ab1ef09
fanlianyou <177688028@qq.com>
2016-07-11 17:45:41 +0800

    初始化项目

---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by lanou3g on 16/7/11.
+//  Copyright © 2016年 fanlianyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weibo/weibo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  weibo
+//
+//  Created by lanou3g on 16/7/11.
+//  Copyright © 2016年 fanlianyou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
